699eb4ea9456463daf08a6bffc934fb8e4588af2
Michael Bailey <mbailey@riotgames.com>
2022-02-02 16:39:14 +0900

    WIP ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,29 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper><InnerBar
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--size': size + '%',
+      }}
+    >{value}</InnerBar></Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: '370px';
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: 5px;
+`;
+const InnerBar = styled.div`
+  width: var(--size);
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
